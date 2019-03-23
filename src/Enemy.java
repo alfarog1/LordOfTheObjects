de5534e6f4c3885636031c597ad236da4d390523
alfarog1 <alfaro.geovanny@gmail.com>
2019-03-23 12:23:21 -0600

    Setters and Getters added

--- src/Enemy.java
+++ src/Enemy.java
@@ -2,18 +2,35 @@
 public class Enemy extends GameCharacter {
 	public Enemy(String type) {
 		if (type.equals("drunk")) {
-			
+			super.setHealth(10);
+			super.setSkill(10);
+			super.setStrength(10);
+			super.se
 		}
 		if (type.equals("bouncer")) {
+			super.setHealth(10);
+			super.setSkill(10);
+			super.setStrength(10);
 			
 		}
 		if (type.equals("homeless guy")) {
+			super.setHealth(10);
+			super.setSkill(10);
+			super.setStrength(10);
 			
 		}
 		if (type.equals("pigeon lady")) {
+			super.setHealth(10);
+			super.setSkill(10);
+			super.setStrength(10);
 			
 		}
-		
+		if (type.equals("pizza guy")) {
+			super.setHealth(10);
+			super.setSkill(10);
+			super.setStrength(10);
+			
+		}
 		
 		
 	}
---
+++ src/GameCharacter.java
@@ -0,0 +1,33 @@
+
+public class GameCharacter {
+	private int health;
+	private double money;
+	private int skill;
+	private int strength;
+	
+	public int getStrength() {
+		return strength;
+	}
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+	public int getHealth() {
+		return health;
+	}
+	public void setHealth(int health) {
+		this.health = health;
+	}
+	public double getMoney() {
+		return money;
+	}
+	public void setMoney(double money) {
+		this.money = money;
+	}
+	public int getSkill() {
+		return skill;
+	}
+	public void setSkill(int skill) {
+		this.skill = skill;
+	}
+	
+}
